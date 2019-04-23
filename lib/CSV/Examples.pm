6c14382380154c10e4f146dbd3113bffaad8933f
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-23 14:14:01 +0700

    Initial commit

---
+++ lib/CSV/Examples.pm
@@ -0,0 +1,17 @@
+package CSV::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example CSV files
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of CSV files in its per-dist share
+dir.
+
+
+=head1 SEE ALSO
+
+Some modules to parse CSV: L<Text::CSV_XS>
